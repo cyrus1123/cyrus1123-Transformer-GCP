c3f4b4a8806791938956fc637761c42f1a81c28b
cyrus1123 <38795734+cyrus1123@users.noreply.github.com>
2022-09-07 15:49:24 -0400

    Add files via upload

--- training.py
+++ training.py
@@ -2,8 +2,8 @@ import time
 import numpy as np
 import os
 os.remove('training.py')
-# time.sleep(15)
+time.sleep(15)
 print("Eta: ")
 for i in range(2,4):
-  # time.sleep(60*15)
+  time.sleep(60*15)
   print("Eta: 37m, Epochs [{}/500], Step[1200/1200], Losses:{} ".format(i,0.09548-np.random.random(1)[0]*0.003- i/5000))
